84db6cc2aab86c938edcc20b2636cc073561fbc2
waeljawadi <waeljawadi1@gmail.com>
2019-01-22 16:30:13 +0100

    OK

--- src/component/timer/Timer.js
+++ src/component/timer/Timer.js
@@ -1,32 +1,9 @@
 import React from 'react'
 import './style/style.css'
+import Calcul from './Calcul'
 
-
-function timer(milliseconds){
-    //Get hours from milliseconds
-    var hours = milliseconds / (1000*60*60);
-    var absoluteHours = Math.floor(hours);
-    var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
-
-    var minutes = (hours - absoluteHours) * 60;
-    var absoluteMinutes = Math.floor(minutes);
-    var m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
-
-    var seconds = (minutes - absoluteMinutes) * 60;
-    var absoluteSeconds = Math.floor(seconds);
-    var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds; 
-    
-    let array_time = [];
-    array_time.push(h);
-    array_time.push(m);
-    array_time.push(s)
-    return array_time;
-  }
-
-
-
-  const separateur = <span className="verybold">:</span>
-  const result_fonction = timer(100000);
+const separateur = <span className="verybold">:</span>
+const result_fonction = Calcul(100000);
 const Timer = () =>
 {
     return (
@@ -34,6 +11,9 @@ const Timer = () =>
             <span className="verybold one">{result_fonction[0]}</span>{separateur}
             <span className="verybold two">{result_fonction[1]}</span>{separateur}
             <span className="verybold three">{result_fonction[2]}</span>
+            <p></p>
+            <button>Start</button>
+            <button>Stop</button>
         </React.Fragment>
     )
 }
